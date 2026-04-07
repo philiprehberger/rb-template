38b6af37bb247a6a59ea352997d5721700f374c7
Philip Rehberger <me@philiprehberger.com>
2026-04-07 10:19:16 -0600

    Release v0.5.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,11 @@ and this gem adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.htm
 
 ## [Unreleased]
 
+## [0.5.1] - 2026-04-07
+
+### Added
+- `truncate` built-in filter with optional length argument (defaults to 30)
+
 ## [0.5.0] - 2026-04-05
 
 ### Added
--- lib/philiprehberger/template/filters.rb
+++ lib/philiprehberger/template/filters.rb
@@ -13,7 +13,13 @@ module Philiprehberger
         'capitalize' => ->(val) { val.to_s.capitalize },
         'reverse' => ->(val) { val.to_s.reverse },
         'length' => ->(val) { val.respond_to?(:length) ? val.length.to_s : val.to_s.length.to_s },
-        'default' => ->(val, arg = '') { val.nil? || val.to_s.empty? ? arg : val.to_s }
+        'default' => ->(val, arg = '') { val.nil? || val.to_s.empty? ? arg : val.to_s },
+        'truncate' => lambda { |val, arg = '30'|
+          str = val.to_s
+          limit = arg.to_i
+          limit = 30 if limit <= 0
+          str.length > limit ? "#{str[0, limit]}..." : str
+        }
       }.freeze
 
       @custom = {}
